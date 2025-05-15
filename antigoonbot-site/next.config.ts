6581e4d866f9e98546a43bef7ed9aa092abd115b
SyntaxXXX <161494574+MrBWC@users.noreply.github.com>
2025-05-15 21:18:48 +0200

    /dc and /github added

--- antigoonbot-site/next.config.ts
+++ antigoonbot-site/next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
-
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  async redirects() {
+    return [
+      {
+        source: '/dc',
+        destination: 'https://discord.gg/WUrPzTWTJf', // replace this
+        permanent: true, // Use false for temporary (307), true for permanent (308)
+      },
+      {
+        source: '/github',
+        destination: 'https://github.com/MrBWC', // replace this
+        permanent: true, // Use false for temporary (307), true for permanent (308)
+      },
+    ]
+  },
+}
 
 export default nextConfig;
