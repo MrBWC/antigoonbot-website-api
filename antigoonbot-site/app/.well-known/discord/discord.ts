86f63d9b2a10770066669d36ed2a00b81a07a0cc
SyntaxXXX <161494574+MrBWC@users.noreply.github.com>
2025-05-15 21:02:42 +0200

    discord.ts

---
+++ antigoonbot-site/app/.well-known/discord/discord.ts
@@ -0,0 +1,7 @@
+// pages/.well-known/discord.ts
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  res.setHeader('Content-Type', 'text/plain')
+  res.send('dh=5cf24350a32700d8ae76a3d3118d3a95392552bb')
+}
