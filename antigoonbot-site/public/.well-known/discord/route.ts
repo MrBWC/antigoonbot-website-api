700c29674d6d06033dfa6cf28b6a0429ba773901
SyntaxXXX <161494574+MrBWC@users.noreply.github.com>
2025-05-15 21:10:27 +0200

    sdf

--- antigoonbot-site/public/.well-known/discord/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from "next/server";
-import type { NextApiRequest, NextApiResponse } from 'next'
-
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.setHeader('Content-Type', 'text/plain')
-  res.send('dh=5cf24350a32700d8ae76a3d3118d3a95392552bb')
-}
